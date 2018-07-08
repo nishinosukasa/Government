a0f6a195cd41a48a02aae1a5721080534a0e00e9
NishinoTsukasa <35717703+nishinosukasa@users.noreply.github.com>
2018-07-08 19:43:29 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,83 @@
+#!/bin/bash 
+clear
+red='\e[1;31m'
+kiddies='\e[1;31m'
+sleep 2
+echo -e $kiddies"##==================================================>" | lolcat
+echo " Author : Siwanna " | lolcat
+sleep 2
+echo -e $kiddies"##==================================================>" | lolcat
+echo " Coder : Siwanna " | lolcat
+sleep 2
+echo -e $kiddies"##==================================================>" | lolcat
+echo " Copyright : Siwanna " | lolcat
+sleep 2
+echo -e $kiddies"##==================================================>" | lolcat
+echo " Blog : crusnos.blogsplot.com " | lolcat
+sleep 2
+echo -e $kiddies"##==================================================>" | lolcat
+echo " website : nostalgiaxploit.wordpress.com " | lolcat
+sleep 2
+echo -e $kiddies"##==================================================>" | lolcat
+echo " contack : nostalgiaxploit@gmail.com " | lolcat
+sleep 2
+echo -e $kiddies"##==================================================>" | lolcat
+echo " Team : NostalgiaXploit " | lolcat
+sleep 2
+echo -e $kiddies"##==================================================>" | lolcat
+echo "You Choose Yes Or No !!! "
+echo "1) yes "
+echo "2) no "
+sleep 1
+read -p "root@install ~#" Variant;
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo "Again Loading..."
+# modules
+# modules php
+apt-get install php
+
+# perl
+apt-get install perl
+
+# python
+apt-get install python
+apt-get install python2
+apt-get install python-pip
+pip install requests
+pip2 install requests
+pip2 install cfscrape
+pip2 install future
+
+# wget
+apt-get install wget
+
+# git
+apt-get install git
+
+# defacer id
+apt-get install nodejs
+pip install defid_notifier
+pip2 install defid_notifier
+pip3 install defid_notifier
+
+# curl
+apt-get install curl
+
+# sudo
+pkg install git ncurses-utils
+git clone https://github.com/st42/termux-sudo.git
+cd termux-sudo
+cat sudo > /data/data/com.termux/files/usr/bin/sudo
+chmod 700 /data/data/com.termux/files/usr/bin/sudo
+clear
+echo "Done Sudah Terinstall Stah "
+echo "Usage : ./Variant"
+fi
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+echo "Okay Thanks You"
+sleep 1
+clear
+fi
